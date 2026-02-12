089878ea3a4a229fe854947d1437c398dd49c0a5
mayank arya <mayanksunny786@gmail.com>
2026-02-12 11:46:03 +0530

    fixed use-callback import

--- frontend/src/product-create/productForm.jsx
+++ frontend/src/product-create/productForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import API from "../services/api";
 import "./productForm.css";
 import { useNavigate } from "react-router-dom";
